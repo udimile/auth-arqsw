d44b8aa9541db71797629321a4cb3dce9de6ca28
Luis Valdo <luisvso.dev@gmail.com>
2025-11-23 20:55:45 -0300

    feat(UserRepository): Adicionado o findByUsername e findByRole no repository para query pelo username e role'

--- src/main/java/br/ucsal/auth/user/model/repository/UserRepository.java
+++ src/main/java/br/ucsal/auth/user/model/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package br.ucsal.auth.user.model.repository;
 
 import br.ucsal.auth.user.model.entity.User;
+import br.ucsal.auth.user.model.entity.UserRole;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByLogin(String login);
+    UserDetails findByUsername(String username);
+
+    List<User> findByRole(UserRole role);
 }
